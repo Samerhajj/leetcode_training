15baa67ded1efa2f6d3a597135e342162ab97dc5
samerhajj <samerhaj44@gmail.com>
2023-07-10 17:41:11 +0300

    TwoSum With java HashTable one with input provided in leetcode one without https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/

---
+++ Java/TwoSumHashTable.java
@@ -0,0 +1,27 @@
+package Java;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSumHashTable {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashTable = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int compliment = target - nums[i];
+            if (hashTable.containsKey(compliment)) {
+                return new int[]{hashTable.get(compliment), i};
+            } else {
+                hashTable.put(nums[i], i);
+            }
+        }
+
+        return new int[]{-1};
+    }
+
+
+    public static void main(String[] args) {
+        int[] test = {2, 7, 11,15};
+        System.out.println(Arrays.toString(twoSum(test, 13)));
+    }
+
+}
