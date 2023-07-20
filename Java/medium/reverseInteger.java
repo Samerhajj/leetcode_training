a388ef5042d8c7ea1cf410794bc62a4c53a33dbd
samerhajj <samerhaj44@gmail.com>
2023-07-20 17:47:47 +0300

    reverse integer and avoid overflow

---
+++ Java/medium/reverseInteger.java
@@ -0,0 +1,30 @@
+package Java.medium;
+
+
+/**
+ * Given a signed 32-bit integer x, return x with its digits reversed.
+ * If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+ */
+public class reverseInteger {
+    public static int reverse(int x) {
+        int result = 0;
+
+        while (x != 0)
+        {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result)
+            { return 0; }
+            result = newResult;
+            x = x / 10;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args){
+        System.out.println(reverse(1534236469));
+
+    }
+}
